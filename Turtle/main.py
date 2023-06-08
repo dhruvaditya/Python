31f45bf593196ed483d7eabe4fa254132306da4e
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2023-06-08 14:27:47 +0530

    Added a project of hand pattern recognization

---
+++ Turtle/main.py
@@ -0,0 +1,17 @@
+import turtle
+from turtle import Turtle,Screen
+timmy=Turtle()
+print(timmy)
+timmy.shape("turtle")
+turtle.right(10)
+turtle.forward(40)
+turtle.right(10)
+turtle.backward(40)
+turtle.backward(20)
+turtle.left(20)
+turtle.backward(10)
+turtle.forward(30)
+
+my_screen=Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
